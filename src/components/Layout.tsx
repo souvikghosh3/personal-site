30049521c2afeca63566d8234acb20220200e6f0
Souvik Ghosh <ghoshsouvik215@gmail.com>
2023-05-01 17:22:11 -0700

    Migrate js -> tsx. Create a new directory for resume page in prepartion of adding further data.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import Navbar from './Navbar'
 import '../styles/global.css'
 
-export default function Layout({ children }) {
+export default function Layout(props: React.PropsWithChildren<unknown>) {
   return (
     <div className="layout">
         <Navbar />
-        <div className="content">
-            { children }
-        </div>
+        <main>{ props.children }</main>
     </div>
-  )
+  );
 }
